c4b705518caaab9a2ddae6ae3b64c6f6df9651ae
Shams109000 <shamsalam804@gmail.com>
2024-11-02 12:55:13 +0530

    add css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Board from './component/Board';
-import Board1 from './component/Board1';
 
 function App() {
   return (
     <div className="App">
       <Board></Board>
-      {/* <Board1></Board1> */}
     </div>
   );
 }
